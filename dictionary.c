6b5fd98968c6d3a51c2bec5af6f67018193875ec
Jacob Hirzel <hirzel@hotmail.com>
2016-06-18 13:25:22 +0000

    Created basic dictionary load loop.

--- dictionary.c
+++ dictionary.c
@@ -7,9 +7,19 @@
  * Implements a dictionary's functionality.
  */
 
+#include <stdio.h>
 #include <stdbool.h>
 
 #include "dictionary.h"
+
+
+// define struct for trie to hold dictionary words
+typedef struct dictionary_node
+{
+    bool is_word; // flag to say if a node is the end of a word
+    struct dictionary_node* children[27]; // pointer to child node array
+} dictionary_node;
+
 
 /**
  * Returns true if word is in dictionary else false.
@@ -25,8 +35,28 @@ bool check(const char* word)
  */
 bool load(const char* dictionary)
 {
-    // TODO
-    return false;
+    // open file and error check
+    FILE* dictionary_ptr = fopen(dictionary, "r");
+    if (dictionary_ptr == NULL)
+    {
+        return false;
+    }
+    else
+    {
+        // declare root instance of the struct
+        // dictionary_node* root;
+        
+        // declare a temp variable to hold each word
+        char dictionary_word[46] = {0};
+        
+        // loop through the dictionary
+        while (fscanf(dictionary_ptr, "%s", dictionary_word) != EOF)
+        {
+            // TEST print the word
+            printf("%s\n", dictionary_word);
+        }
+    }
+    return true;
 }
 
 /**
